c13b2c09d04f8b11125a41c0e19ffefea06c966a
letmediem <103685824+letmediem@users.noreply.github.com>
2022-09-21 08:55:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+a = int(input("Введи"))
+b = int(input("Введи"))
+c = int(input("Введи"))
+if (a <= 3) and (a > 1):
+    print(a)
+else:
+    print("а не входит в промежуток")
+if (b <= 3) and (c > 1):
+    print(b)
+else:
+    print("b не входит в промежуток")
+if (c <= 3) and (c > 1):
+    print(c)
+else:
+    print("c не входит в промежуток")
+
+
